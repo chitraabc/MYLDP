539ee8853ef484efe7e8765f5c27ddec27470748
chitragona <chitra.gona@zemosolabs.com>
2021-03-10 09:48:38 +0530

    Karate api tests in maven project intellij

--- APIAutomation/karate-config.js
+++ APIAutomation/karate-config.js
@@ -5,13 +5,12 @@ function fn() {
        petURLExtenstion : 'v2/pet/findByStatus',
        catURL : 'https://api.thecatapi.com/v1',
        jsonValidationUrl : 'https://jsonplaceholder.typicode.com',
-       oauthTokenUrl : 'https://reqres.in/api/login'
+       oauthTokenUrl : 'https://reqres.in/api/login',
+       graphqlURL : 'https://api.spacex.land/graphql'
   };
   
   
-  {
-    graphqlURL : 'https://api.spacex.land/graphql'
-  };
+
   karate.configure;
   return config;
 
